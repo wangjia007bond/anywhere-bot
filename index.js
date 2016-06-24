c2437aa8f22d39e494f4b4b332624d02a1cb7ad3
jiahao <wangjia007bond@163.com>
2016-06-24 17:11:32 +0800

    test

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 var express = require('express');
 var wechat = require('wechat');
-var crypto = require('crypto');
 var config = require('./config.js');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-var checkSignature = function(query, token) {
-    var signature = query.signature;
-    var timestamp = query.timestamp;
-    var nonce = query.nonce;
-
-    var shasum = crypto.createHash('sha1');
-    var arr = [token, timestamp, nonce].sort();
-    shasum.update(arr.join(''));
-
-    return shasum.digest('hex') === signature;
-};
-
-app.get('/', function(req, res) {
-    if (checkSignature(req.query, config.token)) {
-        res.send(req.query.echostr);
-    } else {
-        res.send('error');
-    }
-});
+app.set('port', (80 || process.env.PORT));
 
 app.use('/wechat', wechat(config, function(req, res, next) {
 	var message = req.weixin;
