faeea827e33aa33033f8763ebd987fd1a04ecf0b
jiahao <wangjia007bond@163.com>
2016-07-02 00:52:11 +0800

    updated to wit 4.0

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ let log = require('node-wit').log;
 // Wit.ai parameters
 const WIT_TOKEN = process.env.WIT_TOKEN || 'XWQB622ZWQXIYPBKISLSIKFDYHFQ7G23';
 
+// ----------------------------------------------------------------------------
+// Wit.ai bot specific code
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {wcid: wechatUserId, context: sessionState}
+const sessions = {};
+
+const findOrCreateSession = (wcid) => {
+	let sessionId;
+	// Let's see if we already have a session for the user wcid
+	Object.keys(sessions).forEach(k => {
+		if (sessions[k].wcid === wcid) {
+			// Yep, got it!
+			sessionId = k;
+		}
+	});
+	if (!sessionId) {
+		// No session found for user wcid, let's create a new one
+		sessionId = new Date().toISOString();
+		sessions[sessionId] = {wcid: wcid, context: {}};
+	}
+	return sessionId;
+};
+
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
