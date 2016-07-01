de8e43c79bf8d52beefd7734e2241b9171cfc4f8
jiahao <wangjia007bond@163.com>
2016-07-02 00:58:18 +0800

    updated to wit 4.0

--- index.js
+++ index.js
@@ -35,6 +35,36 @@ const findOrCreateSession = (wcid) => {
 	return sessionId;
 };
 
+// Our bot actions
+const actions = {
+	send({sessionId}, {text}) {
+		// Our bot has something to say!
+		// Let's retrieve the Wechat user whose session belongs to
+		const recipientId = sessions[sessionId].wcid;
+		if (recipientId) {
+			// Yay, we found our recipient!
+			// Let's forward our bot response to her.
+			// We return a promise to let our bot know when we're done sending
+			return wcMessage(recipientId, text)
+				.then(() => null)
+				.catch((err) => {
+					console.error(
+						'Oops! An error occurred while forwarding the response to',
+						recipientId,
+						':',
+						err.stack || err
+					);
+				});
+		} else {
+			console.error('Oops! Couldn\'t find user for session:', sessionId);
+			// Giving the wheel back to our bot
+			return Promise.resolve()
+		}
+	},
+	// You should implement your custom actions here
+	// See https://wit.ai/docs/quickstart
+};
+
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
