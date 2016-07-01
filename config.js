400d5d9809185623c1e45b7434d5a29803b9b47e
jiahao <wangjia007bond@163.com>
2016-07-02 00:30:29 +0800

    updated to wit 4.0

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 var config = {
     token: 'anywherechatbot',
     appid: 'wx37b4ab67fae5718b',
-    encodingAESKey: 'Ey8ra5vyadvUjeiHlslEpg95Lik7Ct5DNur4GNNwK1R',
-    wit-token: 'XWQB622ZWQXIYPBKISLSIKFDYHFQ7G23'
+    encodingAESKey: 'Ey8ra5vyadvUjeiHlslEpg95Lik7Ct5DNur4GNNwK1R'
 };
 module.exports = config;
