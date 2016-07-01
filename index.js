c40455cc571c038f41390575b152815080c35d35
jiahao <wangjia007bond@163.com>
2016-07-02 00:42:09 +0800

    updated to wit 4.0

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var config = require('./config.js');
 let Wit = require('node-wit').Wit;
 let log = require('node-wit').log;
 
+// Wit.ai parameters
+const WIT_TOKEN = process.env.WIT_TOKEN || 'XWQB622ZWQXIYPBKISLSIKFDYHFQ7G23';
 
 var app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -17,6 +19,8 @@ app.use('/wechat', wechat(config, function(req, res, next) {
 	console.log(res);
 	console.log(req);
 	console.log(message);
+	console.log(WIT_TOKEN);
+	console.log(message.Content);
 	res.reply({type: "text", content: 'Hello world!'});
 }));
 
