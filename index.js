b58669f60684c4a2b4ad22d267370a7d830d86d4
jiahao <wangjia007bond@163.com>
2016-07-02 01:01:29 +0800

    updated to wit 4.0

--- index.js
+++ index.js
@@ -45,16 +45,16 @@ const actions = {
 			// Yay, we found our recipient!
 			// Let's forward our bot response to her.
 			// We return a promise to let our bot know when we're done sending
-			return wcMessage(recipientId, text)
-				.then(() => null)
-				.catch((err) => {
-					console.error(
-						'Oops! An error occurred while forwarding the response to',
-						recipientId,
-						':',
-						err.stack || err
-					);
-				});
+			// return wcMessage(recipientId, text)
+			// 	.then(() => null)
+			// 	.catch((err) => {
+			// 		console.error(
+			// 			'Oops! An error occurred while forwarding the response to',
+			// 			recipientId,
+			// 			':',
+			// 			err.stack || err
+			// 		);
+			// 	});
 		} else {
 			console.error('Oops! Couldn\'t find user for session:', sessionId);
 			// Giving the wheel back to our bot
