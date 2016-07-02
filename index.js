3cacc7ac5b5e6a90091024ca71e88098c542ea11
jiahao <wangjia007bond@163.com>
2016-07-02 08:25:46 +0800

    updated to wit 4.0

--- index.js
+++ index.js
@@ -76,6 +76,33 @@ app.use('/wechat', wechat(config, function(req, res, next) {
 	const sessionId = findOrCreateSession(sender);
 
 	const text = message.Content;
+
+	// We received a text message
+
+	// Let's forward the message to the Wit.ai Bot Engine
+	// This will run all actions until our bot has nothing left to do
+	// wit.runActions(
+	// 	sessionId, // the user's current session
+	// 	text, // the user's message
+	// 	sessions[sessionId].context // the user's current session state
+	// ).then((context) => {
+	// 	// Our bot did everything it has to do.
+	// 	// Now it's waiting for further messages to proceed.
+	// 	console.log('Waiting for next user messages');
+
+	// 	// Based on the session state, you might want to reset the session.
+	// 	// This depends heavily on the business logic of your bot.
+	// 	// Example:
+	// 	// if (context['done']) {
+	// 	//   delete sessions[sessionId];
+	// 	// }
+
+	// 	// Updating the user's current session state
+	// 	sessions[sessionId].context = context;
+	// })
+	// .catch((err) => {
+	// 	console.error('Oops! Got an error from Wit: ', err.stack || err);
+	// });
 }));
 
 app.listen(app.get('port'), function() {
