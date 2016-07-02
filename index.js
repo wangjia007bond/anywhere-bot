c34b8109f55c63b6fb2c2c41add51259c2829ca4
jiahao <wangjia007bond@163.com>
2016-07-02 11:32:46 +0800

    updated to wit 4.0

--- index.js
+++ index.js
@@ -37,34 +37,36 @@ const findOrCreateSession = (wcid) => {
 
 // Our bot actions
 const actions = {
-  send(sessionId, text) {
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      // We return a promise to let our bot know when we're done sending
-      console.log(text);
-      // return fbMessage(recipientId, text)
-      // .then(() => null)
-      // .catch((err) => {
-      //   console.error(
-      //     'Oops! An error occurred while forwarding the response to',
-      //     recipientId,
-      //     ':',
-      //     err.stack || err
-      //   );
-      // });
-    } else {
-      console.error('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      return Promise.resolve()
-    }
+	send(sessionId, text) {
+		// Our bot has something to say!
+		// Let's retrieve the Facebook user whose session belongs to
+		const recipientId = sessions[sessionId].fbid;
+		if (recipientId) {
+	      // Yay, we found our recipient!
+	      // Let's forward our bot response to her.
+	      // We return a promise to let our bot know when we're done sending
+			console.log(text);
+	      // return fbMessage(recipientId, text)
+	      // .then(() => null)
+	      // .catch((err) => {
+	      //   console.error(
+	      //     'Oops! An error occurred while forwarding the response to',
+	      //     recipientId,
+	      //     ':',
+	      //     err.stack || err
+	      //   );
+	      // });
+		} else {
+			console.error('Oops! Couldn\'t find user for session:', sessionId);
+			// Giving the wheel back to our bot
+			return Promise.resolve()
+		}
   },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
 };
+
+const client = new Wit({'XWQB622ZWQXIYPBKISLSIKFDYHFQ7G23', actions});
 
 // Setting up our bot
 // const wit = new Wit({
